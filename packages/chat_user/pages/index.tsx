bf24aba8df5bca571e506ad1e66a74184caa8fe6
Aashish-Dahal <ashishdahal490@gmail.com>
2023-01-24 18:09:46 +0545

    fix: home page

--- packages/chat_user/pages/index.tsx
+++ packages/chat_user/pages/index.tsx
@@ -1,44 +1,44 @@
-import Head from "next/head"
-import { Button, auth } from "@project/shared"
-import { theme } from "@project/shared/src/theme"
-import styled from "styled-components"
+// import Head from "next/head"
+// import { Button, auth } from "@project/shared"
+// import styled from "styled-components"
 import PrivateRoute from "../withPrivateRoute"
-import { signOut } from "firebase/auth"
-import { useContext } from "react"
-import { AuthContext } from "../utils/AuthContext"
+// import { signOut } from "firebase/auth"
+// import { useContext } from "react"
+// import { AuthContext } from "../utils/AuthContext"
+import SideNavBar from "../components/organisms/SideNavBar"
 
-const Container = styled.section`
-  padding: 2rem 2.5rem;
-  display: flex;
-  justify-content: space-between;
-`
+// const Container = styled.section`
+//   padding: 2rem 2.5rem;
+//   display: flex;
+//   justify-content: space-between;
+// `
 
 function Home() {
-  const { setUser } = useContext(AuthContext)
+  // const { setUser } = useContext(AuthContext)
 
-  const handleSignOut = () => {
-    signOut(auth)
-    setUser(null)
-  }
-
-  return (
-    <>
-      <Head>
-        <title>{"HomePage | Next Owner Skeleton"}</title>
-      </Head>
-      <Container>
-        <h1 className={"title"}>{"Home Page"}</h1>
-        <Button
-          minheight={40}
-          fontSize={"16px"}
-          bordercolor={theme.alert}
-          onClick={handleSignOut}
-        >
-          {"Log Out"}
-        </Button>
-      </Container>
-    </>
-  )
+  // const handleSignOut = () => {
+  //   signOut(auth)
+  //   setUser(null)
+  // }
+  return <SideNavBar />
+  // return (
+  //   <>
+  //     <Head>
+  //       <title>{"HomePage | Next Owner Skeleton"}</title>
+  //     </Head>
+  //     <Container>
+  //       <h1 className={"title"}>{"Home Page"}</h1>
+  //       <Button
+  //         minheight={40}
+  //         fontSize={"16px"}
+  //         bordercolor={theme.alert}
+  //         onClick={handleSignOut}
+  //       >
+  //         {"Log Out"}
+  //       </Button>
+  //     </Container>
+  //   </>
+  // )
 }
 
 export default PrivateRoute(Home)
