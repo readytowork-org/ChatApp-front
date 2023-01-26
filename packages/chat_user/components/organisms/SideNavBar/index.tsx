711d68a9b59dc89cf09c171d45b381eeda6c343b
Aashish-Dahal <ashishdahal490@gmail.com>
2023-01-26 16:31:31 +0545

    fix: minor issue

--- packages/chat_user/components/organisms/SideNavBar/index.tsx
+++ packages/chat_user/components/organisms/SideNavBar/index.tsx
@@ -1,28 +1,31 @@
-import React, { useState } from "react"
+import React from "react"
 import Boy from "../../../public/assets/icons/boy.svg"
 import Girl from "../../../public/assets/icons/girl.svg"
 import styled from "styled-components"
 import { Layout, Menu } from "antd"
 import { InputBox } from "../Footer"
 import { MenuTheme } from "antd/lib/menu"
+import InputField from "../../atoms/InputField"
 
 const { Sider, Content } = Layout
 interface ISideNavBarProps {
   theme?: MenuTheme
+  minWidth?: string
 }
 
-const SideNavBar: React.FC<ISideNavBarProps> = ({ theme }) => {
-  const [currentNavbar, setCurrentNavbar] = useState("")
+const SideNavBar: React.FC<ISideNavBarProps> = ({ theme, minWidth }) => {
   const Body = styled.div`
     height: "100vh";
+
     .ant-layout-sider-children {
       height: 100vh;
+      margin-left: 10px;
+      margin-top: 10px;
     }
     .ant-menu-dark.ant-menu-dark:not(.ant-menu-horizontal)
       .ant-menu-item-selected {
       background-color: #1890ff;
       border-radius: 6px;
-      margin-left: 5px;
     }
     .ant-menu-item {
       margin-top: 20px;
@@ -30,50 +33,52 @@ const SideNavBar: React.FC<ISideNavBarProps> = ({ theme }) => {
     .ant-menu-dark.ant-menu-inline .ant-menu-item {
       width: 95%;
     }
+    .h1 {
+      color: #ffffff;
+      font-size: 20px;
+      font-weight: bold;
+    }
   `
   const friends = [
     {
       key: "1",
       icon: <Boy />,
+      name: "hello",
       label: "user 1",
     },
     {
       key: "2",
       icon: <Girl />,
+      name: "hello",
       label: "user 2",
     },
     {
       key: "3",
       icon: <Boy />,
+      name: "hello",
       label: "user 3",
     },
     {
       key: "4",
       icon: <Girl />,
+      name: "hello",
       label: "user 4",
     },
     {
       key: "5",
       icon: <Boy />,
+      name: "hello",
       label: "user 5",
     },
   ]
-  const handleNavigation = (path: any) => {
-    setCurrentNavbar(path["key"])
-  }
 
   return (
     <Body>
       <Layout>
-        <Sider>
-          <Menu
-            theme={theme ?? "dark"}
-            mode={"inline"}
-            key={currentNavbar}
-            onClick={(e) => handleNavigation(e)}
-            items={friends}
-            defaultSelectedKeys={["1"]}
-          ></Menu>
+        <Sider width={minWidth ?? "250px"}>
+          <h1 className={"h1"}>{"Chat App"}</h1>
+          <InputField borderRadius={"5px"} placeHolder={"Search messages"} />
+          <Menu theme={theme ?? "dark"} mode={"inline"} items={friends}></Menu>
         </Sider>
         <Layout>
           <Content
