2c63ac52e59a084337bc2a3764f47c63c4185380
Aashish-Dahal <ashishdahal490@gmail.com>
2023-01-27 18:14:41 +0545

    fix: footer component

--- packages/chat_user/components/organisms/Footer/index.tsx
+++ packages/chat_user/components/organisms/Footer/index.tsx
@@ -1,12 +1,51 @@
 import React from "react"
 import { Layout } from "antd"
 import InputField from "../../atoms/InputField"
+import IconButton from "../../atoms/IconButton"
+import { LinkOutlined, SendOutlined, SmileOutlined } from "@ant-design/icons"
+
+interface IFooterProps {
+  prefixIconClick?: React.MouseEventHandler<HTMLElement>
+  suffixIconClick?: React.MouseEventHandler<HTMLElement>
+}
 
 const { Footer } = Layout
-export const InputBox: React.FC = () => {
+export const FooterComponent: React.FC<IFooterProps> = ({
+  prefixIconClick,
+  suffixIconClick,
+}) => {
   return (
-    <Footer>
-      <InputField />
+    <Footer
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+      }}
+    >
+      <InputField
+        width={"100vh"}
+        prefix={
+          <IconButton
+            onClick={prefixIconClick}
+            icon={<LinkOutlined />}
+          ></IconButton>
+        }
+        suffix={
+          <IconButton
+            onClick={suffixIconClick}
+            icon={<SmileOutlined />}
+          ></IconButton>
+        }
+      />
+      {/* <IconButton
+        icon={ */}
+      <SendOutlined
+        style={{
+          fontSize: "40px",
+        }}
+      />
+      {/* }
+      ></IconButton> */}
     </Footer>
   )
 }
