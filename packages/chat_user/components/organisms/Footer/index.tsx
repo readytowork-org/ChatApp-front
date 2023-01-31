118728eec8e6077902278bb6b01e7fbd64145237
Aashish-Dahal <ashishdahal490@gmail.com>
2023-01-31 17:05:38 +0545

    fix: added formik & input props

--- packages/chat_user/components/organisms/Footer/index.tsx
+++ packages/chat_user/components/organisms/Footer/index.tsx
@@ -1,51 +1,66 @@
+import { useFormik } from "formik"
+import * as yup from "yup"
 import React from "react"
 import { Layout } from "antd"
 import InputField from "../../atoms/InputField"
 import IconButton from "../../atoms/IconButton"
-import { LinkOutlined, SendOutlined, SmileOutlined } from "@ant-design/icons"
+import { LinkOutlined, SmileOutlined } from "@ant-design/icons"
+import EmojiPicker from "../../atoms/EmojiPicker"
 
+interface MessageType {
+  message: string
+}
 interface IFooterProps {
   prefixIconClick?: React.MouseEventHandler<HTMLElement>
   suffixIconClick?: React.MouseEventHandler<HTMLElement>
+  setMessage?: any
+  messageList?: any[]
 }
 
 const { Footer } = Layout
 export const FooterComponent: React.FC<IFooterProps> = ({
   prefixIconClick,
   suffixIconClick,
+  setMessage,
+  messageList,
 }) => {
+  const handleSubmit = () => {
+    setMessage(
+      messageList.concat({
+        incoming: false,
+        message: formik.values.message,
+      })
+    )
+    console.log(formik.values.message)
+  }
+  const formik = useFormik<MessageType>({
+    initialValues: {
+      message: "",
+    },
+    onSubmit: handleSubmit,
+  })
   return (
-    <Footer
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-      }}
-    >
-      <InputField
-        width={"100vh"}
-        prefix={
-          <IconButton
-            onClick={prefixIconClick}
-            icon={<LinkOutlined />}
-          ></IconButton>
-        }
-        suffix={
-          <IconButton
-            onClick={suffixIconClick}
-            icon={<SmileOutlined />}
-          ></IconButton>
-        }
-      />
-      {/* <IconButton
-        icon={ */}
-      <SendOutlined
-        style={{
-          fontSize: "40px",
-        }}
-      />
-      {/* }
-      ></IconButton> */}
+    <Footer>
+      <form onSubmit={formik.handleSubmit}>
+        <InputField
+          width="100%"
+          name={"message"}
+          onChange={formik.handleChange}
+          value={formik.values.message}
+          prefix={
+            <IconButton
+              onClick={prefixIconClick}
+              icon={<LinkOutlined />}
+            ></IconButton>
+          }
+          suffix={
+            <IconButton
+              onClick={suffixIconClick}
+              icon={<SmileOutlined />}
+            ></IconButton>
+          }
+        />
+      </form>
     </Footer>
   )
 }
